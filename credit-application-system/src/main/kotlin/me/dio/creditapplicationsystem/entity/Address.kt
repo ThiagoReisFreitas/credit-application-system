d8bc73a5dfdfe062cdc73b77d1442ee83a2ea8c8
Thiago Reis Freitas <reiscorithi@gmail.com>
2024-02-24 13:19:03 -0300

    Criação do Repository

---
+++ credit-application-system/src/main/kotlin/me/dio/creditapplicationsystem/entity/Address.kt
@@ -0,0 +1,13 @@
+package me.dio.creditapplicationsystem.entity
+
+import jakarta.persistence.Embeddable
+import jakarta.persistence.Column
+
+@Embeddable
+data class Address(
+    @Column(nullable = false)
+    var zipCode: String = "",
+
+    @Column(nullable = false)
+    var street: String = ""
+)
---
+++ credit-application-system/src/main/kotlin/me/dio/creditapplicationsystem/entity/Customer.kt
@@ -0,0 +1,44 @@
+package me.dio.creditapplicationsystem.entity
+
+import jakarta.persistence.CascadeType
+import jakarta.persistence.Column
+import jakarta.persistence.Embedded
+import jakarta.persistence.Entity
+import jakarta.persistence.FetchType
+import jakarta.persistence.GeneratedValue
+import jakarta.persistence.GenerationType
+import jakarta.persistence.Id
+import jakarta.persistence.OneToMany
+import jakarta.persistence.Table
+
+
+@Entity
+@Table(name = "Customer")
+data class Customer(
+    @Column(nullable = false)
+    var firstName: String = "",
+
+    @Column(nullable = false)
+    var lastName: String = "",
+
+    @Column(nullable = false, unique = true)
+    val cpf: String,
+
+    @Column(nullable = false, unique = true)
+    var email: String = "",
+
+    @Column(nullable = false)
+    var password: String = "",
+
+    @Embedded
+    @Column(nullable = false)
+    var address: Address = Address(),
+
+    @Column(nullable = false)
+    @OneToMany(fetch = FetchType.LAZY, cascade = arrayOf(CascadeType.REMOVE, CascadeType.PERSIST), mappedBy = "customer")
+    var credits: List<Credit> = mutableListOf(),
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    val id: Long? = null
+)
